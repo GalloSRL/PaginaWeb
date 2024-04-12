7326ea4f4df1531d8ceb242109a5dbc4a124bcae
GalloSRL <gallosrl.informatica1@gmail.com>
2024-04-12 11:29:03 -0400

    Agregar conexion con la DB

--- G_PANEL/modelos/conexion.php
+++ G_PANEL/modelos/conexion.php
@@ -1,12 +1,12 @@
 <?php
 class Conexion{
 	static public function conectar(){
-		/*$link = new PDO("mysql:host=localhost;dbname=gallosrl_web",
+		$link = new PDO("mysql:host=localhost;dbname=gallosrl_web",
                         "gallosrl_jportillo",
-                        "vjd+PjZON?~w");*/
-		$link = new PDO("mysql:host=localhost;dbname=gallo_panel",
+                        "vjd+PjZON?~w");
+		/*$link = new PDO("mysql:host=localhost;dbname=gallo_panel",
                         "root",
-                        "");
+                        "");*/
 		$link->exec("set names utf8");
 		return $link;
 	}
